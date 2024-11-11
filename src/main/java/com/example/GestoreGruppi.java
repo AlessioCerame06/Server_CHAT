6db82e8a73577a742f3e452ca2b1f920478ae795
lai2-24 <lai2-24@itismeucci.com>
2024-11-11 13:46:40 +0100

    progetto inizializzato

---
+++ src/main/java/com/example/GestoreGruppi.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class GestoreGruppi {
+    private ArrayList<Gruppo> gruppi= new ArrayList<>();
+
+    public GestoreGruppi() {
+    }
+
+    
+}
---
+++ src/main/java/com/example/ServerThread.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread {
+    private Socket socket;
+    private GestoreGruppi gg;
+    
+    public ServerThread(Socket s, GestoreGruppi gg) {
+        this.socket = s;
+        this.gg = gg;
+    }
+
+    public void run(){
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // ascolto                                                                              // (ricevere)
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // parla (invia)
+            String messaggioRicevuto;
+            do {
+                
+
+            } while ( !messaggioRicevuto.equals("CLOSE CONNECTION"));
+            socket.close();
+            System.out.println("Qualcuno si è scollegato");
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+    
+}
